abf1d4b2a332c2b635487a0d175af1474ea661fc
DongDong <jjes0107@gmail.com>
2025-07-17 00:15:01 +0900

    정수버전

--- StreamsSideBar.py
+++ StreamsSideBar.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def Draw_sidebar():
     with st.sidebar:
-        st.header("스트림스 설명")
+        st.header("스트림스 소개")
         st.page_link("StreamsExplanation.py", label="설명 영상 보기")
         st.header("스트림스 버전 설정")
         st.page_link("Streams.py", label="기본 버전")
