86e298b1e9008efb7fedf48dfe080e7f6fb5d6a3
DongDong <jjes0107@gmail.com>
2025-07-16 22:30:26 +0900

    사이드화면 숨김처리

--- app.py
+++ app.py
@@ -15,15 +15,16 @@ pages = {
     "보드게임": [
         # 그룹의 첫 번째 페이지를 default=True로 설정하면 '보드게임' 클릭 시 이 페이지가 먼저 보입니다.
         st.Page("Streams.py", title="스트림스", default=True),
-        # 아래 페이지들은 title을 지정하지 않으면 상단 메뉴에는 보이지 않지만,
-        # st.page_link를 통해 이동할 수 있는 '공식 페이지'로 등록됩니다.
+    ],
+    
+        st.Page("Dice.py", title="주사위 모음")
+    ,
+    "_사이드_화면": [
+       # st.page_link를 통해 이동할 수 있는 '공식 페이지'로 등록됩니다.
         st.Page("StreamsZ.py"),
         st.Page("StreamsR.py")
+    ],
 
-    ],
-    "기타": [
-        st.Page("Dice.py", title="주사위 모음")
-    ],
 }
 
 # 3. 네비게이션 UI 생성
