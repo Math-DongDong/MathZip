2843765a35a8719ebc118484b15403892d0d01bd
DongDong <jjes0107@gmail.com>
2025-07-16 23:30:50 +0900

    사이드바 재구성

--- StreamsSideBar.py
+++ StreamsSideBar.py
@@ -5,4 +5,5 @@ def Draw_sidebar():
         st.header("스트림스 버전 설정")
         st.page_link("Streams.py", label="기본 버전")
         st.page_link("StreamsZ.py", label="정수 버전")
+        st.page_link("StreamsQ.py", label="유리수 버전")
         st.page_link("StreamsR.py", label="실수 버전")
