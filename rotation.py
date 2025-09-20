ab19998bf633feca4f7c8a88e703f73dd00a2965
DongDong <jjes0107@gmail.com>
2025-09-20 18:02:57 +0000

    rotation.py 파일 추가 및 회전체 탐구 제목 설정

---
+++ rotation.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("🔄 회전체 탐구")
+
+st.components.v1.iframe("https://script.google.com/macros/s/AKfycbyV79CCI1MWFW7wTu7N4BqgA1DTeBvVLDwIXOWfBGXrfbdvUFw6iwQAu6Wwq05RRR9jkg/exec", height=600)
