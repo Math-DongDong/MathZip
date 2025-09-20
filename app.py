c204fbe029d88608b18f30a39de4dafe630b66b0
DongDong <jjes0107@gmail.com>
2025-09-20 12:44:38 +0000

    상단 페이지 정의 수정 및 rotation.py 파일 추가

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ st.set_page_config(
 # 2. 상단 페이지들 정의
 # 각 페이지의 실제 콘텐츠는 별도의 파일에 존재합니다.
 pages = {
+    "중1 수학": [
+        # 그룹의 첫 번째 페이지를 default=True로 설정하면 '중1 수학' 클릭 시 이 페이지가 먼저 보입니다.
+        st.Page("rotation.py", title="회전체 탐구", default=True),
+    ],
     "보드게임": [
-        # 그룹의 첫 번째 페이지를 default=True로 설정하면 '보드게임' 클릭 시 이 페이지가 먼저 보입니다.
-        st.Page("Streams.py", title="스트림스", default=True),
+        st.Page("Streams.py", title="스트림스"),
     ],
     "주사위 모음": [
         st.Page("Dice.py", title="주사위 모음")
