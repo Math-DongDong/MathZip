b2e2fb7c42d4bc6208606b8130be9b1466d1e101
DongDong <jjes0107@gmail.com>
2025-09-21 03:17:07 +0900

    아이프레임 변경

--- rotation.py
+++ rotation.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("🔄 회전체 탐구")
 
-st.components.v1.iframe("https://script.google.com/macros/s/AKfycbyV79CCI1MWFW7wTu7N4BqgA1DTeBvVLDwIXOWfBGXrfbdvUFw6iwQAu6Wwq05RRR9jkg/exec", height=600)
+st.components.v1.iframe("https://script.google.com/macros/s/AKfycbzZECzNou9ZTKhVZqK7EtkTpt_DmKj5RQYtPb_f4w_ChP6uaHte4w07wDKVPUyPSG6mXA/exec", height=600)
