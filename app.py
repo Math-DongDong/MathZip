f1339b1323b2e71403ce0005394206181d82a100
DongDong <jjes0107@gmail.com>
2026-03-10 00:35:38 +0000

    중학수학 분리

--- app.py
+++ app.py
@@ -18,21 +18,11 @@ pages = {
     ],
     "보드게임": [
         st.Page("./보드게임/Streams.py", title="스트림스"),
-        st.Page("./보드게임/PigGame.py", title="Pig Game"),
+        st.Page("./보드게임/PigGame.py", title="Pig Game"), 
+        st.Page("./보드게임/#NumberBaseball.py", title="숫자야구"),
         st.Page("./보드게임/Dice.py", title="주사위 모음")
     ],
-    "인공지능 수학": [ 
-        st.Page("./인공지능수학/ImageExpression.py", title="이미지 데이터의 표현"),
-        st.Page("./인공지능수학/ImageConversion.py", title="이미지 데이터의 변환"),
-        st.Page("./인공지능수학/ImageClassification.py", title="이미지 데이터의 분류"),
-        st.Page("./인공지능수학/TextExpression.py", title="텍스트 데이터의 표현과 주제어 찾기"),
-        st.Page("./인공지능수학/TextConversion.py", title="텍스트 데이터에서 유용한 정보 찾기"),
-
-    ],
-    "산업수학": [
-        st.Page("./산업수학/MedicalData.py", title="의료 데이터와 건강 상태"),
-        st.Page("./산업수학/DistanceOptimization.py", title="원자력 발전소 기중기의 이동 경로 최적화"),
-    ],    
+    
 }
 
 # 3. 네비게이션 UI 생성(메뉴바 위치)
